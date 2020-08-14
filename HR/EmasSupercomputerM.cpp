82dcfcf7f988fee920ccec0e23427725c641dc0c
SlickGeek <jenrruhndz@gmail.com>
2020-08-14 00:27:38 -0600

    I want to delete the files I supposed move to folder

--- HR/EmasSupercomputerM.cpp
+++ HR/EmasSupercomputerM.cpp
@@ -7,6 +7,7 @@ int twoPluses(vector<string> grid) {
 
     for(int i = 1; i < grid.size() - 1; i++)
         for(int j = 1; j < grid[0].length() - 1; j++){
+
             int c = 0, c2 = 0;
             if(grid[i][j] == 'G'){
                 //LEFT-CHECK
@@ -16,12 +17,9 @@ int twoPluses(vector<string> grid) {
                     }
                     c++;
                 }
-                cout << "left: " << "i = " << i << ", j = " << j << ", c = " << c << ", c2 = " << c2 << endl;
 
-                if(c == 0){
-                    cout << endl;
+                if(c == 0)
                     continue;
-                }
 
                 //RIGHT-CHECK
                 for(int k = j + 1; k < grid[0].size(); k++){
@@ -32,42 +30,32 @@ int twoPluses(vector<string> grid) {
                 }
                 if(c2 == 0){
                     c = c2;
-                cout << "right: " << "i = " << i << ", j = " << j << ", c = " << c << ", c2 = " << c2 << endl;
-                    cout << endl;
                     continue;
                 }else{
                     c = min(c, c2);
                     c2 = 0;
-                cout << "right: " << "i = " << i << ", j = " << j << ", c = " << c << ", c2 = " << c2 << endl;
                 }
 
                 //UP-CHECK
                 for(int k = i - 1; k >= 0; k--){
-                    if(grid[k][j] == 'B'){
+                    if(grid[k][j] == 'B')
                         break;
-                    }
                     c2++;
                 }
                 if(c2 == 0){
                     c = c2;
-                cout << "up: " << "i = " << i << ", j = " << j << ", c = " << c << ", c2 = " << c2 << endl;
-                    cout << endl;
                     continue;
                 }else{
                     c = min(c, c2);
                     c2 = 0;
-                cout << "up: " << "i = " << i << ", j = " << j << ", c = " << c << ", c2 = " << c2 << endl;
                 }
 
                 //DOWN-CHECK
                 for(int k = j - 1; k < grid.size(); k++){
-                    if(grid[k][j] == 'B'){
+                    if(grid[k][j] == 'B')
                         break;
-                    }
                     c2++;
                 }
-                cout << "down: " << "i = " << i << ", j = " << j << ", c = " << c << ", c2 = " << c2 << endl;
-                cout << endl;
             }
 
             c = min(c, c2) * 4 + 1;
@@ -78,9 +66,6 @@ int twoPluses(vector<string> grid) {
             }else if(c > secondBiggest)
                 secondBiggest = c;
         }
-
-    cout << "b = " << biggest << endl;
-    cout << "bb = " << secondBiggest << endl;
 
     return biggest * secondBiggest;
 
@@ -93,8 +78,6 @@ int main(){
     freopen("myInput.txt", "r", stdin);
     scanf("%d", &n);
     scanf("%d", &m);
-    cout << n << endl;
-    cout << m << endl;
 
     vector<string> sVec;
     for(int i = 0; i < n; i++){
@@ -105,7 +88,7 @@ int main(){
 
     int r = twoPluses(sVec);
 
-    cout << "answer = "<< r << endl;
+    cout << r << endl;
 
     return 0;
 }
